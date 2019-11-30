23d711a879691b62173c1dd4da87a2ca3d7bf023
Carlos Gonzalez <cagl18@gmail.com>
2019-11-30 05:52:54 -0500

    adding build folder to repo

---
+++ build/precache-manifest.6a4e00d885d84bd58a7d287e78530e60.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd2dafc4152217d0455bd2a2219d3ef",
+    "url": "/edohibachi/index.html"
+  },
+  {
+    "revision": "9f4f87b284add97ba0df",
+    "url": "/edohibachi/static/css/main.9310ff62.chunk.css"
+  },
+  {
+    "revision": "a45e4bb3770edb130cb7",
+    "url": "/edohibachi/static/js/2.25adf443.chunk.js"
+  },
+  {
+    "revision": "9f4f87b284add97ba0df",
+    "url": "/edohibachi/static/js/main.1275653e.chunk.js"
+  },
+  {
+    "revision": "969534d1d3627ec0f674",
+    "url": "/edohibachi/static/js/runtime~main.cdeea48e.js"
+  },
+  {
+    "revision": "939d445e9151bc638aab21799b9eeb50",
+    "url": "/edohibachi/static/media/specialty_Hibachi-Shrimp.939d445e.png"
+  },
+  {
+    "revision": "9077967cba6694d43eefefa58fc50e8e",
+    "url": "/edohibachi/static/media/specialty_Hibachi-Vegetable.9077967c.png"
+  },
+  {
+    "revision": "4c6970afa95399a2eb89bce84efd786f",
+    "url": "/edohibachi/static/media/specialty_Salt-and-Pepper-Chicken.4c6970af.png"
+  },
+  {
+    "revision": "7d801722bcb9fe4deee10ef89f11f8f8",
+    "url": "/edohibachi/static/media/specialty_Steak-bowl.7d801722.png"
+  },
+  {
+    "revision": "c4124cc62eaa828d7d2874832f26c95e",
+    "url": "/edohibachi/static/media/specialty_godzilla-roll.c4124cc6.png"
+  },
+  {
+    "revision": "5fcb5374b20f4cf3f0236ba6ab6f0c30",
+    "url": "/edohibachi/static/media/specialty_gyoza.5fcb5374.png"
+  },
+  {
+    "revision": "2b169ad9394a4a5eba04b1e0ac71d6c8",
+    "url": "/edohibachi/static/media/sushi-hero.2b169ad9.jpg"
+  },
+  {
+    "revision": "c2a99b067fb5c0922918b2e58917ee93",
+    "url": "/edohibachi/static/media/sushi.c2a99b06.jpg"
+  }
+]);

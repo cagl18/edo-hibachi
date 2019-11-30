0a95271ae9e8fadf019e9ae80513027292094891
Carlos Gonzalez <cagl18@gmail.com>
2019-11-30 06:11:52 -0500

    Updates

---
+++ precache-manifest.2f006a5d50574895949d544debcb9ac3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30d3317581ff0cd95c80cb292e8bd80",
+    "url": "/edohibachi/index.html"
+  },
+  {
+    "revision": "b8a9d52dd309de5b5e84",
+    "url": "/edohibachi/static/css/main.9310ff62.chunk.css"
+  },
+  {
+    "revision": "75877f10aaee9bd8ead1",
+    "url": "/edohibachi/static/js/2.0db7a503.chunk.js"
+  },
+  {
+    "revision": "b8a9d52dd309de5b5e84",
+    "url": "/edohibachi/static/js/main.caf587c2.chunk.js"
+  },
+  {
+    "revision": "dcd25327a4e82f7b1a4b",
+    "url": "/edohibachi/static/js/runtime~main.80a46177.js"
+  },
+  {
+    "revision": "939d445e9151bc638aab21799b9eeb50",
+    "url": "/edohibachi/static/media/specialty_Hibachi-Shrimp.939d445e.png"
+  },
+  {
+    "revision": "9077967cba6694d43eefefa58fc50e8e",
+    "url": "/edohibachi/static/media/specialty_Hibachi-Vegetable.9077967c.png"
+  },
+  {
+    "revision": "4c6970afa95399a2eb89bce84efd786f",
+    "url": "/edohibachi/static/media/specialty_Salt-and-Pepper-Chicken.4c6970af.png"
+  },
+  {
+    "revision": "7d801722bcb9fe4deee10ef89f11f8f8",
+    "url": "/edohibachi/static/media/specialty_Steak-bowl.7d801722.png"
+  },
+  {
+    "revision": "c4124cc62eaa828d7d2874832f26c95e",
+    "url": "/edohibachi/static/media/specialty_godzilla-roll.c4124cc6.png"
+  },
+  {
+    "revision": "5fcb5374b20f4cf3f0236ba6ab6f0c30",
+    "url": "/edohibachi/static/media/specialty_gyoza.5fcb5374.png"
+  },
+  {
+    "revision": "2b169ad9394a4a5eba04b1e0ac71d6c8",
+    "url": "/edohibachi/static/media/sushi-hero.2b169ad9.jpg"
+  },
+  {
+    "revision": "c2a99b067fb5c0922918b2e58917ee93",
+    "url": "/edohibachi/static/media/sushi.c2a99b06.jpg"
+  }
+]);
